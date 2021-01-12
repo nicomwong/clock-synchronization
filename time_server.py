c8688654f15d51aac354fcf57d3199bb956442e6
Nico Wong <nicowong8@gmail.com>
2021-01-11 20:54:13 -0800

    Made so a thread is spawned to handle sending time to client

--- time_server.py
+++ time_server.py
@@ -3,17 +3,32 @@
 import socket
 import sys
 import time
+import threading
 
 IP = "127.0.0.1"    # Server's IP
 PORT = 2001 # Server's port
 
-def printConnected():
+def printConnected(address):
     timeStruct = time.gmtime(time.time() )
-    print(  "Server connected to (\'" + str(IP) + "\', " + str(PORT) + ")\n" +
+    clientIP, clientPort = address
+    print(  "Server connected to (\'" + str(clientIP) + "\', " + str(clientPort) + ")\n" +
             "second: " + str(timeStruct.tm_sec) + "\n" +
             "minute: " + str(timeStruct.tm_min) + "\n" +
             "hour: " + str(timeStruct.tm_hour) + "\n"
             )
+
+# Sends server time to client
+# Simulates propagation delay
+def sendTimeToClient():
+            
+    time.sleep(delay)   # simulate client->server propagation delay
+    serverTime = time.time()  # time since epoch (unix time)
+    printConnected(address) # print connection info
+    time.sleep(delay)   # simulate server->client propagation delay
+    
+    # send server time to client
+    sock.sendto(str(serverTime).encode(), address)
+
 
 delay = 1   # for simulating propagation delay
 
@@ -26,11 +41,5 @@ while True:
     data, address = sock.recvfrom(1024) # Max. message size is 1024 bytes
 
     if data == b"sync_req":
-        # Print information
-        printConnected()
-        
-        time.sleep(delay)   # simulate client->server propagation delay
-        currTime = time.time()  # time since epoch (unix time)
-        time.sleep(delay)   # simulate server->client propagation delay
-        printConnected()
-        sock.sendto(str(currTime).encode(), address)    # send time to client
+        # Spawn a new thread to handle the clock sync with client
+        threading.Thread(target = sendTimeToClient, args = () ).start()
